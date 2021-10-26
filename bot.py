ec8ed378c1b13f8b3f719fff255b30af7d691460
aure-nogueras <anogueras@correo.ugr.es>
2021-10-26 22:28:46 +0200

    Change the way the recommendations are chosen, based in the day of the year

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import tweepy
 import json
-import random
 import os
-
+from datetime import date
 
 # Twitter authentication
 auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET_KEY'])
@@ -20,29 +19,30 @@ except:
 with open('recommendations.json') as f:
 	data = json.load(f)
 	
-# Generate a random value to choose a recommendation
-recommendation = 0
-index = set(range(len(data)))
-print(index)
-while data[recommendation]['appeared'] == "True" and len(index) > 0:
-	recommendation = int(random.sample(index, 1)[0])
-	index.remove(recommendation)
+# Generate a value to choose a recommendation based in the day of the year
+todays_date = date.today()
+year = todays_date.year
+month = todays_date.month
+day = todays_date.day
+
+days_each_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+leap_year = year % 4 == 0
+
+if leap_year:
+	days_each_month[1] = 29
 	
-if len(index) == 0 and data[recommendation]['appeared'] == "True":
-	print("No hay :(")
-	for i in range(len(data)):
-		print(i)
-		data[i]['appeared'] = "False"
-	recommendation = int(random.uniform(0,len(data)))
+day_of_the_year = 0	
 	
-if data[recommendation]['appeared'] == "False":
-	data[recommendation]['appeared'] = "True"
-		
-# Rewrite the file to change the status of the chosen recommendation
-with open('recommendations.json', 'w') as f:
-	f.write(json.dumps(data, sort_keys=True, indent=4, separators=(',',': ')))
+for i in range(0, month-1, 1):
+	day_of_the_year += days_each_month[i]
 	
+day_of_the_year += day
+
+recommendation = day_of_the_year % len(data)
+
+# Message the bot is going to tweet
 msg = f"The recommendation of the day is a {data[recommendation]['type']} named {data[recommendation]['title']}. {data[recommendation]['plot']}."
-			
+
+# Tweet			
 api.update_status_with_media(msg, data[recommendation]['img'])
 
